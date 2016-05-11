3f8d75b4922e8030cd1194ee1ecd2d39ba00d2f9
Ataíde <ataide.neto31@gmail.com>
2016-05-11 20:17:00 -0300

    HORA DO SHOW

--- main.c
+++ main.c
@@ -7,111 +7,109 @@
 
 volatile int exit_programa = FALSE; //Tambem pode ser while(!exit_program) ali no while, ao invés de !key[KEY_ESC]
 void fecha_programa() {exit_programa=TRUE;}
-int trilho(int * x,int * y){
+int trilho(int x,int y){
     //y = -y;
-    printf ("X:%d Y:%d\n",*x,*y);
+    printf ("X:%d Y:%d\n",x,y);
     //ANDAR NAS CORDENADAS X
 
-    if((*x)==MAX_X){
-        if(((*y)<=MAX_Y && (*y)>=460) || ((*y)<=410 && (*y)>=353) || (*y)==245 || ((*y)<=141 && (*y)>=MIN))
+    if(x==MAX_X){
+        if((y<=MAX_Y && y>=460) || (y<=410 && y>=353) || y==245 || (y<=141 && y>=MIN))
             return 1;
     }
 
-    else if((*x)==512){
-        if(((*y)<=460 && (*y)>=410))
+    else if(x==512){
+        if((y<=460 && y>=410))
             return 1;
     }
 
-    else if((*x)==450){
-        if(((*y)<=460 && (*y)>=MIN))
+    else if(x==450){
+        if((y<=460 && y>=MIN))
             return 1;
     }
 
-    else if((*x)==385){
-        if(((*y)<=460 && (*y)>=410) || ((*y)<=354 && (*y)>=194) || ((*y)<=141 && (*y)>=90))
+    else if(x==385){
+        if((y<=460 && y>=410) || (y<=354 && y>=194) || (y<=141 && y>=90))
             return 1;
     }
 
-    else if((*x)==318){
-        if(((*y)<=MAX_Y && (*y)>=460) || ((*y)<=410 && (*y)>=354) || ((*y)<=194 && (*y)>=141) || ((*y)<=90 && (*y)>=MIN))
+    else if(x==318){
+        if((y<=MAX_Y && y>=460) || (y<=410 && y>=354) || (y<=194 && y>=141) || (y<=90 && y>=MIN))
             return 1;
     }
 
-    else if((*x)==257){
-        if(((*y)<=MAX_Y && (*y)>=460) || ((*y)<=410 && (*y)>=354) || ((*y)<=194 && (*y)>=141) || ((*y)<=90 && (*y)>=MIN))
+    else if(x==257){
+        if((y<=MAX_Y && y>=460) || (y<=410 && y>=354) || (y<=194 && y>=141) || (y<=90 && y>=MIN))
             return 1;
     }
 
-    else if((*x)==187){
-        if(((*y)<=460 && (*y)>=410) || ((*y)<=354 && (*y)>=194) || ((*y)<=141 && (*y)>=90))
+    else if(x==187){
+        if((y<=460 && y>=410) || (y<=354 && y>=194) || (y<=141 && y>=90))
             return 1;
     }
 
-    else if((*x)==123){
-        if(((*y)<=460 && (*y)>=MIN))
+    else if(x==123){
+        if((y<=460 && y>=MIN))
             return 1;
     }
 
-    else if((*x)==61){
-        if(((*y)<=460 && (*y)>=410))
+    else if(x==61){
+        if((y<=460 && y>=410))
             return 1;
     }
 
-    else if((*x)==MIN){
-        if(((*y)<=MAX_Y && (*y)>=460) || ((*y)<=410 && (*y)>=353) || (*y)==245 || ((*y)<=141 && (*y)>=MIN))
+    else if(x==MIN){
+        if((y<=MAX_Y && y>=460) || (y<=410 && y>=353) || y==245 || (y<=141 && y>=MIN))
             return 1;
     }
 
     //ANDAR NAS CORDENADAS Y
 
-    if((*y)==MAX_Y){
-        if((*x)<=MAX_X && (*x)>=MIN)
+    if(y==MAX_Y){
+        if(x<=MAX_X && x>=MIN)
             return 1;
     }
-    else if((*y)==460){
-        if(((*x)<=MAX_X && (*x)>=450) || ((*x)<=385 && (*x)>=318) || ((*x)<=257 && (*x)>=187) || ((*x)<=123 && (*x)>=MIN))
+    else if(y==460){
+        if((x<=MAX_X && x>=450) || (x<=385 && x>=318) || (x<=257 && x>=187) || (x<=123 && x>=MIN))
             return 1;
     }
-    else if((*y)==410){
-        if(((*x)<=MAX_X && (*x)>=512) || ((*x)<=450 && (*x)>=123) || ((*x)<=61 && (*x)>=MIN))
+    else if(y==410){
+        if((x<=MAX_X && x>=512) || (x<=450 && x>=123) || (x<=61 && x>=MIN))
             return 1;
     }
-    else if((*y)==353){
-        if(((*x)<=MAX_X && (*x)>=318) || ((*x)<=257 && (*x)>=MIN) || ((*x)<=61 && (*x)>=MIN))
-            return 1;
-    }
-
-    else if((*y)==300){
-        if(((*x)<=385 && (*x)>=187))
+    else if(y==353){
+        if((x<=MAX_X && x>=318) || (x<=257 && x>=MIN) || (x<=61 && x>=MIN))
             return 1;
     }
 
-    else if((*y)==248){
-        if(((*x)<=MAX_X && (*x)>=385) ||((*x)<=187 && (*x)>=MIN) )
+    else if(y==300){
+        if((x<=385 && x>=187))
             return 1;
     }
 
-    else if((*y)==194){
-        if(((*x)<=385 && (*x)>=187))
+    else if(y==248){
+        if((x<=MAX_X && x>=385) ||(x<=187 && x>=MIN) )
             return 1;
     }
 
-    else if((*y)==141){
-        if(((*x)<=MAX_X && (*x)>=450) || ((*x)<=385 && (*x)>=318) || ((*x)<=257 && (*x)>=187)|| ((*x)<=123 && (*x)>=MIN))
+    else if(y==194){
+        if((x<=385 && x>=187))
             return 1;
     }
 
-    else if((*y)==88){
-        if(((*x)<=MAX_X && (*x)>=MIN))
+    else if(y==141){
+        if((x<=MAX_X && x>=450) || (x<=385 && x>=318) || (x<=257 && x>=187)|| (x<=123 && x>=MIN))
             return 1;
     }
 
-    else if((*y)==MIN){
-        if(((*x)<=MAX_X && (*x)>=318) || ((*x)<=257 && (*x)>=MIN))
+    else if(y==90){
+        if((x<=MAX_X && x>=MIN))
             return 1;
     }
 
-
+    else if(y==MIN){
+        if((x<=MAX_X && x>=318) || (x<=257 && x>=MIN))
+            return 1;
+    }
 
     return 0;
 }
@@ -163,6 +161,7 @@ int main ()
     int i = 0;
     int tempo_andar = clock();
     int tempo_sprite = clock();
+    int tempo_mudar_posicao = clock();
     int pos_x=555,pos_y=515;
     int direcao_atual=2; // 0=direita 1=esquerda 2=cima 3=baixo
     //GAME LOOP
@@ -175,41 +174,44 @@ int main ()
             if(key[KEY_ESC]){
               fecha_programa();
             }
-            if(key[KEY_D]){
-                direcao_atual=0;
 
+            if(key[KEY_D]){
+                if(trilho(pos_x+1,pos_y))
+                    direcao_atual=0;
             }
+
             if(key[KEY_S]){
-                direcao_atual=3;
+                if(trilho(pos_x,pos_y+1))
+                    direcao_atual=3;
             }
             if(key[KEY_A]){
-                direcao_atual=1;
+                if(trilho(pos_x-1,pos_y))
+                    direcao_atual=1;
             }
             if(key[KEY_W]){
-                direcao_atual=2;
+                if(trilho(pos_x,pos_y-1))
+                    direcao_atual=2;
             }
 
-            if(clock() - tempo_andar > 20){
+
+            if(clock() - tempo_andar > 5){
                 if(direcao_atual==0){
-                    pos_x++;
-                    if(!trilho(&pos_x,&pos_y))
-                        pos_x--;
-                }
-                else if(direcao_atual==1){
-                    pos_x--;
-                    if(!trilho(&pos_x,&pos_y))
+                    if(trilho(pos_x+1,pos_y))
                         pos_x++;
                 }
-                else if(direcao_atual==2){
-                    pos_y--;
-                    if(!trilho(&pos_x,&pos_y))
-                        pos_y++;
+                else if(direcao_atual==1){
+                    if(trilho(pos_x-1,pos_y))
+                        pos_x--;
                 }
-                else if (direcao_atual==3){
-                    pos_y++;
-                    if(!trilho(&pos_x,&pos_y))
+                else if(direcao_atual==2){
+                    if(trilho(pos_x,pos_y-1))
                         pos_y--;
                 }
+                else if (direcao_atual==3){
+                    if(trilho(pos_x,pos_y+1))
+                        pos_y++;
+                }
+
                 tempo_andar = clock();
             }
 
