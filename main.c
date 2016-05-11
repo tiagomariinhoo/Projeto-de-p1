d1fb267b174222243605bac82db6d0cf50ecadad
Ataíde <ataide.neto31@gmail.com>
2016-05-11 13:43:54 -0300

    tá ficando monstro

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <allegro.h>
-
+#include <time.h>
+#include <stdio.h>
 volatile int exit_programa = FALSE; //Tambem pode ser while(!exit_program) ali no while, ao invés de !key[KEY_ESC]
 void fecha_programa() {exit_programa=TRUE;}
+int trilho(int x,int y){
+    return 1;
+}
+
 END_OF_FUNCTION(fecha_programa);
 
 int main ()
@@ -19,13 +24,30 @@ int main ()
     set_close_button_callback(fecha_programa);
 
 
+    BITMAP *direita[2];
+    BITMAP *esquerda[2];
+    BITMAP *cima[2];
+    BITMAP *baixo[2];
+
+    direita[0] = load_bitmap("sprites/pacman1.bmp",NULL);
+    direita[1] = load_bitmap("sprites/pacman11.bmp",NULL);
+    esquerda[0] = load_bitmap("sprites/pacman3.bmp",NULL);
+    esquerda[1] = load_bitmap("sprites/pacman33.bmp",NULL);
+    cima[0] = load_bitmap("sprites/pacman4.bmp",NULL);
+    cima[1] = load_bitmap("sprites/pacman44.bmp",NULL);
+    baixo[0] = load_bitmap("sprites/pacman2.bmp",NULL);
+    baixo[1] = load_bitmap("sprites/pacman22.bmp",NULL);
+
 
     BITMAP *buffer = create_bitmap(600,600);
-    BITMAP *pacman = load_bitmap("sprites/pacman.bmp",NULL); //Ponteiro pro Boneco
+    BITMAP *sprite_atual = cima[0]; //Ponteiro pro Boneco
     BITMAP *mapa = load_bitmap("sprites/mapa.bmp",NULL); //Ponteiro pro mapa
 
-
-    int pos_x=110,pos_y=110;
+    int i = 0;
+    int tempo_andar = clock();
+    int tempo_sprite = clock();
+    int pos_x=574,pos_y=530;
+    int direcao_atual=2; // 0=direita 1=esquerda 2=cima 3=baixo
     //GAME LOOP
     while (!exit_programa) //Condição para fechar o programa, array Key[estado da tecla]
     {
@@ -33,31 +55,59 @@ int main ()
 
         //INPUT
         //Detecta as entradas
-            if(key[KEY_ESC])
-            {
+            if(key[KEY_ESC]){
               fecha_programa();
             }
+            if(key[KEY_D]){
+                direcao_atual=0;
 
-            if(key[KEY_D])
-            {
-                //clear(buffer);
-                pos_x++;
             }
-            if(key[KEY_S])
-            {
-                //clear(buffer);
-                pos_y++;
+            if(key[KEY_S]){
+                direcao_atual=3;
             }
-            if(key[KEY_A])
-            {
-                //clear(buffer);
-                pos_x--;
+            if(key[KEY_A]){
+                direcao_atual=1;
             }
-            if(key[KEY_W])
-            {
-                //clear(buffer);
-                pos_y--;
+            if(key[KEY_W]){
+                direcao_atual=2;
             }
+
+            if(clock() - tempo_andar > 5){
+                if(direcao_atual==0){
+                    pos_x++;
+                }
+                else if(direcao_atual==1){
+                    pos_x--;
+                }
+                else if(direcao_atual==2){
+                    pos_y--;
+                }
+                else if (direcao_atual==3){
+                    pos_y++;
+                }
+                tempo_andar = clock();
+            }
+
+            if(clock() - tempo_sprite > 100){
+                i++;
+                if(i>1)
+                    i=0;
+                if(direcao_atual==0){
+                    sprite_atual = direita[i];
+                }
+                else if(direcao_atual==1){
+                    sprite_atual = esquerda[i];
+                }
+                else if(direcao_atual==2){
+                    sprite_atual = cima[i];
+                }
+                else if (direcao_atual==3){
+                    sprite_atual = baixo[i];
+                }
+                tempo_sprite = clock();
+            }
+            //printf ("tempo:%d clock:%d\n",tempo,clock());
+
 
         //UPDATE
         //Atualiza o estado do jogo
@@ -67,9 +117,8 @@ int main ()
         //Parte de desenho - Ponteiro para BITMAP que é o "screen", tudo que vc desenhar, aparece na tela
 
         draw_sprite(buffer,mapa,0,0);
-        draw_sprite_ex(screen,pacman,pos_x,pos_y, DRAW_SPRITE_NORMAL,DRAW_SPRITE_NO_FLIP);
-
-
+        draw_sprite(buffer,sprite_atual,pos_x,pos_y);
+        //draw_sprite_ex(screen,pacman,pos_x,pos_y, DRAW_SPRITE_NORMAL,DRAW_SPRITE_NO_FLIP);
         draw_sprite(screen,buffer,0,0); //Sprite em que eu quero desenhar, buffer na screen
         //clear(buffer);
 
@@ -77,7 +126,7 @@ int main ()
     }
     //FINALIZAÇÃO
     destroy_bitmap(buffer);
-    destroy_bitmap(pacman);
+    destroy_bitmap(sprite_atual);
     destroy_bitmap(mapa);
 
 
